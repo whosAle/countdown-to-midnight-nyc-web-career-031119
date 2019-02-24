0612de9c2fbfcfb11891d36fb29a65fe70843677
A Belgrave <abelgrave95@gmail.com>
2019-02-24 18:40:10 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,24 @@
 #write your code here
 
-def countdown
+def countdown(time)
+  while time > 0
+    puts "#{time} SECOND(S)!"
+    time -= 1
+  end
+
+  "HAPPY NEW YEAR!"
+
 end
+
+def countdown_with_sleep(time)
+  while time > 0
+    puts "#{time} SECOND(S)!"
+    time -= 1
+    sleep(1)
+  end
+
+  "HAPPY NEW YEAR!"
+end
+
+#countdown(20)
+#countdown_with_sleep(10)
